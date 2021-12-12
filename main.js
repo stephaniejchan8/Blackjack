f85ebd023f1e224d6be2e782ae3c6e273b2d1c6e
stephaniejchan8 <stephaniejchan8@gmail.com>
2021-12-12 15:15:11 +1000

    function to randomly select card from deck, deals first 4 cards

--- main.js
+++ main.js
@@ -14,43 +14,93 @@ let cardNameCounter = 0;
 const cardSuit = ['D', 'C', 'H', 'S'];
 
 // Loop to assign properties to 52 "cards"
-for (let i = 0; i < 52; i++) {
+for (let i = 0; i < 52; ++i) {
   // Declare a temp card object
-  const card = new Object();
+  const tempCard = new Object();
 
   // Assign all cards available 
-  card.available = 'available';
+  tempCard.available = 'available';
 
   // Change 'name' of card every 4 cards
   if (i !== 0 && i % 4 === 0) {
     cardNameCounter++;
   }
-  card.name = cardName[cardNameCounter];
+  tempCard.name = cardName[cardNameCounter];
 
   // Assign numerical value to cards: 1-9 is same as number version of 'name'
   if (i < 32) {
-    card.value = +cardName[cardNameCounter];
+    tempCard.value = +cardName[cardNameCounter];
   } // Assign 2 values to ace cards
   else if (i >= 48) {
-    card.value = 1;
-    card.altValue = 11;
+    tempCard.value = 1;
+    tempCard.altValue = 11;
   } // Assign numerical value 10 to cards 10, J, Q, K
   else {
-    card.value = 10;
+    tempCard.value = 10;
   }
 
   // Assign 1 of 4 suits to cards with the same name
   if (i % 4 === 0) {
-    card.suit = cardSuit[0];
+    tempCard.suit = cardSuit[0];
   } else if (i % 4 === 1) {
-    card.suit = cardSuit[1];
+    tempCard.suit = cardSuit[1];
   } else if (i % 4 === 2) {
-    card.suit = cardSuit[2];
+    tempCard.suit = cardSuit[2];
   } else {
-    card.suit = cardSuit[3];
+    tempCard.suit = cardSuit[3];
   }
 
   // Push temp card object to deck array
-  deck.push(card);
+  deck.push(tempCard);
 }
-console.log(deck);
+
+// Declare JS variables for DOM of computer and player cards
+const computerCards = document.querySelector('.computerCards');
+const playerCards = document.querySelector('.playerCards');
+let playerWin = false;
+let computerWin = false;
+
+// 2. Function: randomly select card from array
+//   Create div with object / card's properties
+//   Position according to which player it is, append to container
+//   Remove card from available cards in deck
+
+// Randomly generate an index number of the deck
+function randomCardNo() {
+  // To test randomCardNo function:
+  // for (let i = 0; i < 52; i += 2) {
+  //   deck[i].available = "in play";
+  // }
+  let tempNo = Math.floor(Math.random() * 52);
+  if (deck[tempNo].available === 'available') {
+    console.log('card available');
+    console.log(tempNo);
+    return tempNo;
+  } else {
+    console.log('card in play, try again');
+    return randomCardNo();
+  }
+}
+
+let counter = 0;
+// Function to create cards
+const createCard = (personCards) => {
+  counter++;
+  const card = document.createElement('div');
+  card.classList.add('card');
+  const deckIndex = randomCardNo();
+  card.innerHTML = `<p>${deck[deckIndex].name}${deck[deckIndex].suit}`;
+  if (deck[deckIndex].suit === 'D' || deck[deckIndex].suit === 'H') {
+    card.classList.add('red');
+  }
+  personCards.appendChild(card);
+  deck[deckIndex].available = "in play";
+}
+
+
+// while (!playerWin && !computerWin) {
+  while (counter < 4) {
+    createCard(playerCards);
+    createCard(computerCards);
+  }
+// }
