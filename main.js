d3180cb38b535e1013a90a4d21ed7a3359865e61
stephaniejchan8 <stephaniejchan8@gmail.com>
2021-12-15 13:34:07 +1000

    created function to calculate user's hand value, trying dataset to store card value

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Create array with deck of 52 cards
-//   Loop to automate card name, suit, value, available
-// Object - name, suit, value
+//   Loop to automate card name, suit, cardValue, available
+// Object - name, suit, cardValue
 //   ? available(or you could make 2 arrays - dealing deck array and a base deck array)
 
 // Create array for deck of card objects
@@ -27,16 +27,16 @@ for (let i = 0; i < 52; ++i) {
   }
   tempCard.name = cardName[cardNameCounter];
 
-  // Assign numerical value to cards: 1-9 is same as number version of 'name'
+  // Assign numerical cardValue to cards: 1-9 is same as number version of 'name'
   if (i < 32) {
-    tempCard.value = +cardName[cardNameCounter];
+    tempCard.cardValue = +cardName[cardNameCounter];
   } // Assign 2 values to ace cards
   else if (i >= 48) {
-    tempCard.value = 1;
-    tempCard.altValue = 11;
-  } // Assign numerical value 10 to cards 10, J, Q, K
+    tempCard.cardValue = 1;
+    tempCard.altCardValue = 11;
+  } // Assign numerical cardValue 10 to cards 10, J, Q, K
   else {
-    tempCard.value = 10;
+    tempCard.cardValue = 10;
   }
 
   // Assign 1 of 4 suits to cards with the same name
@@ -57,35 +57,33 @@ for (let i = 0; i < 52; ++i) {
 // Declare JS variables for DOM of computer and player cards
 const computerCards = document.querySelector('.computerCards');
 const playerCards = document.querySelector('.playerCards');
-let playerWin = false;
-let computerWin = false;
 
-// 2. Function: randomly select card from array
-//   Create div with object / card's properties
-//   Position according to which player it is, append to container
-//   Remove card from available cards in deck
+// Create JS objects to store if user has won or is standing
+const player = {
+  win: false,
+  stand: false
+};
+const computer = {
+  win: false,
+  stand: false
+};
 
-// Randomly generate an index number of the deck
+
+// Randomly generate an index number for the deck array
 function randomCardNo() {
-  // To test randomCardNo function:
-  // for (let i = 0; i < 52; i += 2) {
-  //   deck[i].available = "in play";
-  // }
   let tempNo = Math.floor(Math.random() * 52);
+  // Only return an index of a card that is available
   if (deck[tempNo].available === 'available') {
-    console.log('card available');
-    console.log(tempNo);
     return tempNo;
-  } else {
-    console.log('card in play, try again');
+  } // re-run function randomCardNo until an available card is randomly chosen
+  else {
     return randomCardNo();
   }
 }
 
-let counter = 0;
 // Function to create cards
+// Parameter determines which user to assign card to
 const createCard = (personCards) => {
-  counter++;
   const card = document.createElement('div');
   card.classList.add('card');
   const deckIndex = randomCardNo();
@@ -93,14 +91,61 @@ const createCard = (personCards) => {
   if (deck[deckIndex].suit === 'D' || deck[deckIndex].suit === 'H') {
     card.classList.add('red');
   }
+  card.dataset.cardValue = deck[deckIndex].cardValue;
+  if (deck[deckIndex].altCardValue) {
+    card.dataset.altCardValue = deck[deckIndex].altCardValue;
+  }
   personCards.appendChild(card);
+  //   Remove card from available cards in deck
   deck[deckIndex].available = "in play";
 }
 
-
-// while (!playerWin && !computerWin) {
-  while (counter < 4) {
+while (!player.win && !computer.win) {
+  for (let i = 0; i < 2; ++i) {
     createCard(playerCards);
     createCard(computerCards);
   }
-// }
+  // if (playerStand === true) {
+
+  // }
+  // Temporary measure to stop an infinite loop
+  player.win = true;
+}
+
+// 7. Function: calculate each user's total hand cardValue
+// If total = 21 = user wins
+// Allow for 2 + totals(ace card)
+const calcHandValue = (user) => {
+  const cardsArray = document.querySelectorAll(`.${user}Cards .card`);
+  let total = 0;
+  let altTotal = 0;
+  let altAce = false;
+  for (let i = 0; i < cardsArray.length; ++i) {
+    // First attempt:
+    // if (altAce) {
+    //   altTotal += +cardsArray[i].dataset.cardValue;
+    //   console.log('altTotal increased')
+    // }
+    // else if (!altAce && cardsArray[i].dataset.altCardValue) {
+    //   altTotal = total + +cardsArray[i].dataset.altCardValue;
+    //   altAce = true;
+    //   console.log('altTotal created: ', altTotal);
+    // }
+    total += +cardsArray[i].dataset.cardValue;
+    if (cardsArray[i].dataset.altCardValue) {
+      altAce = true;
+    }
+  }
+  console.log(`${user} hand value: `, total);
+  if (altAce) {
+    altTotal = total + 10;
+    console.log(`${user} alt hand value: `, altTotal);
+  }
+  if (total === 21 || altTotal === 21) {
+    // user.win = true; - needs to be fixed
+    // console.log(user.win);
+    console.log(`Blackjack! ${user} won the game!`);
+  }
+}
+calcHandValue("computer");
+calcHandValue("player");
