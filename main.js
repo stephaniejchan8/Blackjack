0bd3f36f8570d72a6f8337052c442299679a8f20
stephaniejchan8 <stephaniejchan8@gmail.com>
2021-12-16 12:00:58 +1000

    working draft version of JS, needs refactoring

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 //   Loop to automate card name, suit, cardValue, available
 // Object - name, suit, cardValue
 //   ? available(or you could make 2 arrays - dealing deck array and a base deck array)
-
+const announcement = document.querySelector('.announcement');
+const hitMe = document.querySelector('.hit');
+const playerStand = document.querySelector('.stand');
 // Create array for deck of card objects
 const deck = [];
 
@@ -60,14 +62,23 @@ const playerCards = document.querySelector('.playerCards');
 
 // Create JS objects to store if user has won or is standing
 const player = {
-  win: false,
-  stand: false
+  name: 'player',
+  win: 'N/A',
+  stand: false,
+  allCards: playerCards.getElementsByClassName('card'),
+  hand: '0',
+  score: 0
 };
 const computer = {
-  win: false,
-  stand: false
+  name: 'computer',
+  win: 'N/A',
+  stand: false,
+  allCards: computerCards.getElementsByClassName('card'),
+  hand: '0',
+  score: 0
 };
-
+const computerHandValue = document.querySelector('.computerHandValue p');
+const playerHandValue = document.querySelector('.playerHandValue p');
 
 // Randomly generate an index number for the deck array
 function randomCardNo() {
@@ -87,65 +98,137 @@ const createCard = (personCards) => {
   const card = document.createElement('div');
   card.classList.add('card');
   const deckIndex = randomCardNo();
+  // Add card's name and suit to the div 'card'
   card.innerHTML = `<p>${deck[deckIndex].name}${deck[deckIndex].suit}`;
+  // Make diamonds and hearts cards red
   if (deck[deckIndex].suit === 'D' || deck[deckIndex].suit === 'H') {
     card.classList.add('red');
   }
+  // Store card's value (and alt value) in a HTML data attribute
   card.dataset.cardValue = deck[deckIndex].cardValue;
   if (deck[deckIndex].altCardValue) {
     card.dataset.altCardValue = deck[deckIndex].altCardValue;
   }
+  // Add card to correct user's side
   personCards.appendChild(card);
   //   Remove card from available cards in deck
   deck[deckIndex].available = "in play";
 }
 
-while (!player.win && !computer.win) {
-  for (let i = 0; i < 2; ++i) {
-    createCard(playerCards);
-    createCard(computerCards);
-  }
-  // if (playerStand === true) {
 
-  // }
-  // Temporary measure to stop an infinite loop
-  player.win = true;
+// Deal first 2 cards to each user
+for (let i = 0; i < 2; ++i) {
+  createCard(playerCards);
+  createCard(computerCards);
+  calcHandValue(computer);
+  calcHandValue(player);
 }
 
 // 7. Function: calculate each user's total hand cardValue
 // If total = 21 = user wins
-// Allow for 2 + totals(ace card)
-const calcHandValue = (user) => {
-  const cardsArray = document.querySelectorAll(`.${user}Cards .card`);
+// Allow for 2 totals(ace card)
+function calcHandValue(user) {
+  const cardsArray = user.allCards;
   let total = 0;
   let altTotal = 0;
   let altAce = false;
   for (let i = 0; i < cardsArray.length; ++i) {
-    // First attempt:
-    // if (altAce) {
-    //   altTotal += +cardsArray[i].dataset.cardValue;
-    //   console.log('altTotal increased')
-    // }
-    // else if (!altAce && cardsArray[i].dataset.altCardValue) {
-    //   altTotal = total + +cardsArray[i].dataset.altCardValue;
-    //   altAce = true;
-    //   console.log('altTotal created: ', altTotal);
-    // }
     total += +cardsArray[i].dataset.cardValue;
     if (cardsArray[i].dataset.altCardValue) {
       altAce = true;
     }
   }
-  console.log(`${user} hand value: `, total);
+  console.log(user.name + `hand value: `, total);
   if (altAce) {
     altTotal = total + 10;
-    console.log(`${user} alt hand value: `, altTotal);
+    console.log(user.name + `alt hand value: `, altTotal);
+    if (altTotal <= 21) {
+      user.hand = `${total} / ${altTotal}`;
+    }
+  } else {
+    user.hand = `${total}`;
   }
-  if (total === 21 || altTotal === 21) {
-    // user.win = true; - needs to be fixed
-    // console.log(user.win);
-    console.log(`Blackjack! ${user} won the game!`);
+  displayValue(user.name);
+  checkWinner(user, total, altTotal);
+}
+
+function displayValue(userName) {
+  if (userName === 'player') {
+    playerHandValue.textContent = player.hand;
+  } else {
+    computerHandValue.textContent = computer.hand;
   }
 }
-calcHandValue("computer");
-calcHandValue("player");
+
+function computerTurn() {
+  if (+computer.hand <= 16) {
+    createCard(computerCards);
+    calcHandValue(computer);
+  } else {
+    computer.stand = true;
+  }
+}
+
+
+
+function calcWinner() {
+  if (computer.win === 'lose') {
+    announceWinner(player);
+  } else if (player.win === 'lose') {
+    announceWinner(computer);
+  } else if (+player.hand > +computer.hand) {
+    announceWinner(player);
+  } else if (+player.hand === +computer.hand) {
+    announceTie();
+  } else {
+    announceWinner(computer);
+  }
+}
+
+function announceTie() {
+  player.win = 'tie';
+  announcement.textContent = "It's a tie!";
+  announcement.classList.add('show');
+}
+
+function announceWinner(user) {
+  user.win = 'win';
+  announcement.textContent = `${user.name} wins!`;
+  announcement.classList.add('show');
+}
+
+
+hitMe.addEventListener('click', function() {
+  createCard(playerCards);
+  calcHandValue(player);
+});
+playerStand.addEventListener('click', function() {
+  disableButtons();
+  player.stand = true;
+  // Continue game if no user has won
+  while (player.win === 'N/A' && computer.win === 'N/A') {
+    computerTurn();
+    if (computer.stand) {
+      calcWinner();
+    }
+  }
+});
+
+function checkWinner(user, total1, total2) {
+  if (total1 === 21 || total2 === 21) {
+    user.win = 'win';
+    disableButtons();
+    announceWinner(user);
+    console.log(`Blackjack!`);
+  } else if (total1 > 21) {
+    user.win = 'lose';
+    disableButtons();
+    calcWinner();
+    console.log(player.name + 'lost');
+  }
+}
+
+function disableButtons() {
+  hitMe.setAttribute('disabled', 'true');
+  playerStand.setAttribute('disabled', 'true');
+}
